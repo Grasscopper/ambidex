9bfd64b4ab3d91521f0a79bef3147ea04ff2511f
Grasscopper <jssstransky@gmail.com>
2021-06-28 14:09:12 -0400

    Persist game data in a database.

--- src/VirtueTestContainer.js
+++ src/VirtueTestContainer.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react'
-import { setItem, getItem } from './myFunctions.js'
+import { Link } from 'react-router-dom'
 import VirtueTestTile from './VirtueTestTile'
+
+import { setItem, getItem } from './myFunctions.js'
+import createPersistedState from 'use-persisted-state'
+
 // https://ambidex-api.herokuapp.com
 const VirtueTestContainer = (props) => {
   const [ambidex, setAmbidex] = useState(null)
 
-  useEffect(() => {
+  const useSelectedGames = createPersistedState('selectedGames')
+  const [selectedGames, setSelectedGames] = useSelectedGames([true, false, false])
+
+  useEffect(() => { //create or use an existing Ambidex Game
     if (getItem('session')) { //return to session
       const session = getItem('session')
       fetch(`https://ambidex-api.herokuapp.com/ambidexes/${session}`)
@@ -24,10 +31,52 @@ const VirtueTestContainer = (props) => {
     }
   }, [])
 
+  let gameOne = "button is-medium is-primary"
+  let gameTwo = "button is-medium"
+  let gameThree = "button is-medium"
+  let characters = []
+
+  if (selectedGames[0]) {
+    gameOne = "button is-medium is-primary"
+    gameTwo = "button is-medium"
+    gameThree = "button is-medium"
+    if (ambidex) characters = ambidex.games[0].characters
+  }
+  else if (selectedGames[1]) {
+    gameOne = "button is-medium"
+    gameTwo = "button is-medium is-primary"
+    gameThree = "button is-medium"
+    if (ambidex) characters = ambidex.games[1].characters
+  }
+  else if (selectedGames[2]) {
+    gameOne = "button is-medium"
+    gameTwo = "button is-medium"
+    gameThree = "button is-medium is-primary"
+    if (ambidex) characters = ambidex.games[2].characters
+  }
+
+  const chooseGame = (event) => {
+    event.preventDefault()
+    switch (event.currentTarget.id) {
+      case "nonary":
+        setSelectedGames([true, false, false])
+        break
+      case "virtue":
+        setSelectedGames([false, true, false])
+        break
+      case "zero":
+        setSelectedGames([false, false, true])
+        break
+      default:
+        setSelectedGames([true, false, false])
+        break
+    }
+  }
+
   let characterTiles = <div></div>
   if (ambidex) {
     characterTiles =
-    ambidex.games[0].characters.map((character) => {
+    characters.map((character) => {
       return (
         <VirtueTestTile character={character} />
       )
@@ -35,7 +84,42 @@ const VirtueTestContainer = (props) => {
   }
 
   return (
-    <ul>{characterTiles}</ul>
+    <div className="index-container">
+      <h1 style={{ color: "#1FD1B2", fontWeight: "bold" }}>Ambidex Game</h1>
+
+      <h2 style={{ border: "none" }}>Choose a game and your player character.</h2>
+      <div className="columns is-multiline">
+
+        <div className="column is-full">
+          <Link to="/">
+            <button className="button is-link">
+              <strong>Back</strong>
+            </button>
+          </Link>
+        </div>
+
+        <div className="character column is-one-third">
+          <button id="nonary" className={gameOne} onClick={chooseGame} style={{ width: "100%" }}>
+            <strong>9 Hours, 9 Persons, 9 Doors</strong>
+          </button>
+        </div>
+
+        <div className="character column is-one-third">
+          <button id="virtue" className={gameTwo} onClick={chooseGame} style={{ width: "100%" }}>
+          <strong>Virtue's Last Reward</strong>
+          </button>
+        </div>
+
+        <div className="character column is-one-third">
+          <button id="zero" className={gameThree} onClick={chooseGame} style={{ width: "100%" }}>
+            <strong>Zero Time Dilemma</strong>
+          </button>
+        </div>
+
+        {characterTiles}
+      </div>
+
+    </div>
   )
 }
 
